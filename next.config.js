dde06b97257e8e5c330954e9f1130fbd94014f15
EDolganov <evgenij.dolganov@gmail.com>
2024-08-26 17:49:29 +0300

    Dep updates

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false, // fix a double useState call. see: https://github.com/vercel/next.js/issues/35822
-  experimental: {
-    appDir: true,
-  },
 }
 
 module.exports = nextConfig
